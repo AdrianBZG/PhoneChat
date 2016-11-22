fd2ae7a54dcd94471bda33f3359888064d6e1e60
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-11-22 07:39:52 +0000

    Move server to https://github.com/EleDiaz/PhoneChatServer-Node avoid duplicate

--- server/models/user-profile.js
+++
@@ -1,7 +0,0 @@
-﻿var UserProfile = function(cnf) {
-    this.email = cnf.email,
-    this.firstName = cnf.firstName,
-    this.lastName = cnf.lastName
-};
-
-module.exports = UserProfile;
--- server/models/user-registration.js
+++
@@ -1,8 +0,0 @@
-﻿var UserRegistration = function(cnf) {
-    this.email = cnf.email,
-    this.firstName = cnf.firstName,
-    this.lastName = cnf.lastName,
-    this.password = cnf.password,
-    this.passwordConfirm = cnf.passwordConfirm
-};
-module.exports = UserRegistration;
