f12c7d1dfb4881cba13e69d9dc37d987d6798b4c
AdrianBZG <arodriba@ull.es>
2016-11-22 15:44:51 +0000

    Adding Fontawesome + Initial sensors page things

--- platforms/browser/www/js/user_registration/settings.js
+++ platforms/browser/www/js/user_registration/settings.js
@@ -1,3 +1,3 @@
 ﻿var PhoneChat = PhoneChat || {};
 PhoneChat.Settings = PhoneChat.Settings || {};
-PhoneChat.Settings.signUpUrl = "http://127.0.0.1:30000/api/account/register";
+PhoneChat.Settings.signUpUrl = "https://phonechat.herokuapp.com/api/account/register";
--- www/js/sensors/phoneSensors.js
+++ www/js/sensors/phoneSensors.js
@@ -1,42 +1,58 @@
-var pictureSource;   // picture source: pictureSource.PHOTOLIBRARY, pictureSource.SAVEDPHOTOALBUM
-var destinationType; // sets the format of returned value
+var pictureSource;          // Picture source: pictureSource.PHOTOLIBRARY, pictureSource.SAVEDPHOTOALBUM
+var destinationType;        // Sets the format of returned value
+var htmlElementToEdit;      // The HTML element to put the info inside
 
 // onGPSSuccess Callback
 //   This method accepts a `Position` object, which contains
 //   the current GPS coordinates
 //
 var onGPSSuccess = function(position) {
-    console.log('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Accuracy: '          + position.coords.accuracy);
+  console.log('Latitude: '          + position.coords.latitude          + '\n' +
+  'Longitude: '         + position.coords.longitude         + '\n' +
+  'Accuracy: '          + position.coords.accuracy);
+
+  var element = document.getElementById(htmlElementToEdit);
+  element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                      'Longitude: '          + position.coords.longitude             + '<br />' +
+                      'Accuracy: '           + position.coords.accuracy              + '<br />';
 };
 
 var onAltitudeSuccess = function(altitude) {
-    console.log('Altitude: '          + position.coords.altitude          + '\n' +
-                'Altitude Accuracy: ' + position.coords.altitudeAccuracy);
+  console.log('Altitude: '          + position.coords.altitude          + '\n' +
+  'Altitude Accuracy: ' + position.coords.altitudeAccuracy);
+
+  var element = document.getElementById(htmlElementToEdit);
+  element.innerHTML = 'Altitude: '           + position.coords.altitude              + '<br />' +
+                      'Altitude Accuracy: '          + position.coords.altitudeAccuracy      + '<br />';
 };
 
 var onOrientationSuccess = function(altitude) {
   console.log('Heading/Orientation: '           + position.coords.heading);
+
+  var element = document.getElementById(htmlElementToEdit);
+  element.innerHTML = 'Heading/Orientation: '           + position.coords.heading              + '<br />';
 };
 
-function getCurrentGPSPosition() {
+function getCurrentGPSPosition(elementId) {
+  htmlElementToEdit = elementId;
   navigator.geolocation.getCurrentPosition(onGPSSuccess, onError);
 }
 
-function getCurrentAltitude() {
+function getCurrentAltitude(elementId) {
+  htmlElementToEdit = elementId;
   navigator.geolocation.getCurrentPosition(onAltitudeSuccess, onError);
 }
 
-function getCurrentOrientation() {
+function getCurrentOrientation(elementId) {
+  htmlElementToEdit = elementId;
   navigator.geolocation.getCurrentPosition(onOrientationSuccess, onError);
 }
 
 // onError Callback receives a PositionError object
 //
 function onError(error) {
-    console.log('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+  console.log('code: '    + error.code    + '\n' +
+  'message: ' + error.message + '\n');
 }
 
 // Camara
@@ -94,27 +110,27 @@ function capturePhoto() {
   // Take picture using device camera and retrieve image as base64-encoded string
   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
     destinationType: destinationType.DATA_URL });
-}
+  }
 
-// A button will call this function
-//
-function capturePhotoEdit() {
-  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-    destinationType: destinationType.DATA_URL });
-}
+  // A button will call this function
+  //
+  function capturePhotoEdit() {
+    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+      destinationType: destinationType.DATA_URL });
+    }
 
-// A button will call this function
-//
-function getPhoto(source) {
-  // Retrieve image file location from specified source
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-    destinationType: destinationType.FILE_URI,
-    sourceType: source });
-}
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+      }
 
-// Called if something bad happens.
-//
-function onFail(message) {
-  alert('Failed because: ' + message);
-}
+      // Called if something bad happens.
+      //
+      function onFail(message) {
+        alert('Failed because: ' + message);
+      }
