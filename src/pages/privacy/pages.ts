394a7c772250e49ed94376c78945c4151d7fca3b
AdrianBZG <arodriba@ull.es>
2016-12-18 23:31:54 +0000

    Account Settings page finished #21

--- src/pages/privacy/pages.ts
+++ src/pages/privacy/pages.ts
@@ -5,30 +5,16 @@ import { Login } from '../login/pages';
 
 import { AppService } from '../../services/app.service';
 
-declare var $;
-
 @Component({
   styles: ['ion-icon { display: block; text-align: center; font-size: 10em; }'],
   templateUrl: 'template.html',
 })
 export class Settings {
-  public userIp: any;
-  public userName: any;
-  public userId: any;
-
   constructor(
       public navCtrl: NavController
     , public navParams: NavParams
     , public appService: AppService
     ) {}
-
-  ionViewDidEnter() {
-    this.userName = this.appService.user;
-    this.userId = this.appService.userId;
-    $.get("http://ipinfo.io", function(response) {
-      this.userIp = response.ip;
-    }, "jsonp");
-  }
 
   disconnect() {
     this.appService.disconnectChat();
