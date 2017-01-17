156c5c9a58101692478a440adaa901970633fc89
AdrianBZG <arodriba@ull.es>
2017-01-17 00:51:57 +0000

    Created Imgur service to upload images #11

---
+++ src/services/imgur.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {AppService} from "./app.service";
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/map";
+
+declare var $;
+
+@Injectable()
+export class ImgurService {
+   public token: any;
+   private clientId = 'fd6a24a6d80d25f';
+    constructor(private http: Http, private appService: AppService) {
+      this.token = this.extractToken(document.location.hash);
+    }
+
+    uploadImage(fileUrl: any) {
+      let auth;
+      if (this.token) auth = 'Bearer ' + this.token;
+      else auth = 'Client-ID ' + this.clientId;
+
+      $.ajax({
+          url: 'https://api.imgur.com/3/image',
+          type: 'POST',
+          headers: {
+            Authorization: auth,
+            Accept: 'application/json'
+          },
+          data: {
+            image: fileUrl,
+            type: 'base64'
+          },
+          success: function(result) {
+            var id = result.data.id;
+            console.log('URL de la imagen: ' + id);
+          }
+        });
+      }
+
+    extractToken(hash) {
+      var match = hash.match(/access_token=(\w+)/);
+      return !!match && match[1];
+    }
+}
